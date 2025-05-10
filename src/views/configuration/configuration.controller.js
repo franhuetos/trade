f43bca1fd97bf79771f5f65ee2032d20edcb170d
franhuetos <fran.huetos@gmail.com>
2025-05-10 10:22:59 +0200

    fix: async jsonbin

--- src/views/configuration/configuration.controller.js
+++ src/views/configuration/configuration.controller.js
@@ -115,7 +115,7 @@ async function addNewItemHandler(){
 	location.reload();
 };
 
-function configClickHandler(){
+async function configClickHandler(){
 	const dataUrl = document.getElementById('dataUrl').value;
 	const masterKey = document.getElementById('masterKey').value;
 	if (window.confirm("Estas seguro de querer configurar estos datos?. Perderás la información guardada en localStorage.")) {
@@ -124,7 +124,7 @@ function configClickHandler(){
 			url: dataUrl,
 			masterKey: masterKey
 		});
-		let response = JSONBIN.get();
+		let response = await JSONBIN.get();
 		StoreService.set(UtilsService.CONSTANT.SESSION.CHART_DATA, response);
 	}
 }
